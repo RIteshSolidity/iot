05f948302c159ae53270da1082e21ec629178531
Ritesh Modi <desiredstate@outlook.com>
2018-03-21 02:06:51 -0400

    commit

---
+++ DeviceIdentity/DeviceIdentity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+
+namespace DeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=mclass21.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=32+rp32aGG0pyMd8v9zsrW0gU2C7Ic5BN9f0lh1HLjQ=";
+       
+        
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+
+        }
+
+        private static async Task AddDeviceAsync() {
+            string deviceID = "mydeviceid1";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceID));
+            }
+            catch (DeviceAlreadyExistsException ex)
+            {
+                device = await registryManager.GetDeviceAsync(deviceID);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+
+        }
+    }
+}
---
+++ DeviceIdentity/Receiver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
+
+
+
+
+namespace Receiver
+{
+    class Program
+    {
+       // static string connectionString = "Endpoint=sb://foriot.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fz/j2tfbhC5I6jSwe3ywHripH4K1m/seWFBXjQJaN4U=";
+       // static string connectionString = "Endpoint=sb://mclassns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ELD51oBaxbL+7KAED36XDTcuVPYdcPm7epuMNDxLLys=";
+         static string connectionString = "HostName=mclass21.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=32+rp32aGG0pyMd8v9zsrW0gU2C7Ic5BN9f0lh1HLjQ=";
+         static string iotHubD2cEndpoint = "messages/events";
+       // static string iotHubD2cEndpoint = "allcritical";
+        static EventHubClient eventHubClient;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+    }
+}
